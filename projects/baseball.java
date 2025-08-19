8b2009786db0de526a4c35ee019ac44268b66563
Jeph Humphrey <155738167+hetch-4@users.noreply.github.com>
2025-08-19 08:18:53 +0000

    Create baseball.java
    'baseball score calculator '

---
+++ projects/baseball.java
@@ -0,0 +1,32 @@
+import java.util.*;
+ class baseball {
+    public static int calPoints(String[] ops) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String op : ops) {
+            if (op.equals("+")) {
+                int top = stack.pop();
+                int newScore = top + stack.peek();
+                stack.push(top);      // put the top back
+                stack.push(newScore); // push the new score
+            } else if (op.equals("D")) {
+                stack.push(2 * stack.peek());
+            } else if (op.equals("C")) {
+                stack.pop();
+            } else {
+                stack.push(Integer.parseInt(op));
+            }
+        }
+
+        int sum = 0;
+        for (int score : stack) {
+            sum += score;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        String[] ops = {"5", "2", "C", "D", "+"};
+        System.out.println(calPoints(ops)); // Output: 30
+    }
+}
