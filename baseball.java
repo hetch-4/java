f356f1f7bea60f7aea0682562cb09ea932970ec8
Jeph Humphrey <155738167+hetch-4@users.noreply.github.com>
2025-08-26 17:27:27 +0000

    Update baseball.java

--- baseball.java
+++ baseball.java
@@ -1,35 +1,32 @@
 import java.util.*;
- class baseball {
-    public static int calPoints(String[] ops) {
-        Stack<Integer> stack = new Stack<>();
 
-        for (String op : ops) {
-            if (op.equals("+")) {
-                int top = stack.pop();
-                int newScore = top + stack.peek();
-                stack.push(top);      // put the top back
-                stack.push(newScore); // push the new score
-            } else if (op.equals("D")) {
-                stack.push(2 * stack.peek());
-            } else if (op.equals("C")) {
-                stack.pop();
-            } else {
-                stack.push(Integer.parseInt(op));
-            }
+class  Main{
+  public static void main(String [] args){
+    System.out.println("Hello world!");
+  }
+  
+  public static void calPoints(String Pnt){
+    Stack<int> stack = new Stack<>();
+      
+      for(int ops:stack){
+        if(ops=="+"){
+          int top = stack.pop();
+          
+          int point = top.peek() + top;
+          stack.push(top);
+          stack.push(point);
+        }else if(ops=="D"){
+          stack.push(2*stack.peek());
+        }else if(ops=="C"){
+          stack.pop();
+        }else{
+          stack.push(ops);
         }
-
         int sum = 0;
-        for (int score : stack) {
-            sum += score;
+        for (int ops:  stack){
+          sum += ops;
         }
         return sum;
-    }
-
-    public static void main(String[] args) {
-        String[] ops = {"5", "2", "C", "D", "+"};
-        System.out.println(calPoints(ops)); // Output: 30
-        String[] op = {"5", "-2", "4", "C", "D", "9", "+", "+"};
-        System.out.println(calPoints(op));
-        
-    }
+      }
+    
 }
