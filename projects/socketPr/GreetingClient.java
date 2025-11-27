802b52a5e31109d25ccc9bcf123b4fd52a8930ee
hetch-4 <humphreydola@gmail.com>
2025-11-27 00:47:33 -0500

    client side

--- projects/socketPr/GreetingClient.java
+++ projects/socketPr/GreetingClient.java
@@ -7,17 +7,17 @@ public class GreetingClient{
         int port = Integer.parseInt(args[1]);
 
         try{
-            System.out.println("Connecting to"+serverName+"on port"+port);
+            System.out.println("Connecting to "+serverName+" on port "+port);
             Socket client = new Socket(serverName, port);
 
             System.out.println("Just connected to "+client.getRemoteSocketAddress());
             OutputStream outToServer = client.getOutputStream();
             DataOutputStream out = new DataOutputStream(outToServer);
 
-            out.writeUTF("Hello from"+client.getLocalSocketAddress());
+            out.writeUTF("Hello from "+client.getLocalSocketAddress());
 
             DataInputStream in = new DataInputStream(client.getInputStream());
-            System.out.println("Server says:"+in.readUTF());
+            System.out.println("Server says: "+in.readUTF());
 
             client.close();
         }catch(IOException e){
